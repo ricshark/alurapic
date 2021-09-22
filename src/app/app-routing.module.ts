0c92bf764ff3e9d90d562a88db14282f845686de
Ricardo Souza <Ricardo.Souza@smartm.com>
2021-09-22 11:38:44 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
-/*ricardo*/
 const routes: Routes = [
   {path: 'user/:userName',
   component: PhotoListComponent,
